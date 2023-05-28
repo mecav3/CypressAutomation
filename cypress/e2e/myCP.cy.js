5e4b4c70eda96525485dfad9d71ea1ba8a67c238
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-05-28 23:45:13 +0300

    3

--- cypress/e2e/myCP.cy.js
+++ cypress/e2e/myCP.cy.js
@@ -11,9 +11,23 @@ cy.get('.btn').click();
 
 cy.get('#navbarDropdown > span').should('contain','51');
  cy.get(':nth-child(2) > .nav-link > .title').click();
- cy.get('h3').should('contain','User Management');
 
- cy.get('#user_groups').click();
+ cy.get('h3').then((label)=>{
+    expect(label).to.have.text('User Management');
+ })
+
+ cy.get('span > .btn').click();
+
+ cy.get(':nth-child(1) > .col-12 > .form-group > .form-control').type('myname123@yes.com');
+ cy.get('.form-body > :nth-child(2) > :nth-child(1) > .form-group > .form-control').type('myname123@yes.com');
+ 
+cy.get('#user_group_id').select('Students');
+
+it('new one',()=>{
+    cy.get('.form-body > :nth-child(2) > :nth-child(2) > .form-group > .form-control').type('myname123@yes.com');
+    cy.log('write');
+})
+
     })
 
      
