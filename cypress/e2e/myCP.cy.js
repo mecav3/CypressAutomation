4b01c6071b3d5f09ad8e06626b99621cde274918
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-05-28 22:57:07 +0300

    sec

--- cypress/e2e/myCP.cy.js
+++ cypress/e2e/myCP.cy.js
@@ -1,12 +1,22 @@
  /// <reference types="cypress" />
  
  describe('my first',()=>{
+
     it('login yo Library',()=>{
+
 cy.visit('https://library1.cydeo.com/login.html');
 cy.get(':nth-child(3) > label').type('librarian51@library');
 cy.get(':nth-child(4) > label').type('libraryUser');
 cy.get('.btn').click();
 
 cy.get('#navbarDropdown > span').should('contain','51');
+ cy.get(':nth-child(2) > .nav-link > .title').click();
+ cy.get('h3').should('contain','User Management');
+
+ cy.get('#user_groups').click();
     })
+
+     
+    
+
  })
