d571150429081a436a0b34e359f136f342c89c7d
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-05-28 18:23:06 +0300

    first

--- cypress/e2e/myCP.cy.js
+++ cypress/e2e/myCP.cy.js
@@ -3,5 +3,10 @@
  describe('my first',()=>{
     it('login yo Library',()=>{
 cy.visit('https://library1.cydeo.com/login.html');
+cy.get(':nth-child(3) > label').type('librarian51@library');
+cy.get(':nth-child(4) > label').type('libraryUser');
+cy.get('.btn').click();
+
+cy.get('#navbarDropdown > span').should('contain','51');
     })
  })
